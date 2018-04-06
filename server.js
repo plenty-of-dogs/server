f5ecf2f0ae785a620b1b6f823bb69f09447819f5
Jimmy Chang <jimmyfchang94@gmail.com>
2018-04-06 09:57:09 -0700

    create account works

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict'
 
 //Application dependencies
-
 const express = require('express');
 const pg = require('pg');
 const cors = require('cors');
@@ -9,15 +8,10 @@ const bodyParser = require('body-parser');
 const superagent = require('superagent');
 
 //application setup
-
 const app = express();
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
 const TOKEN = process.env.TOKEN;
-const PORT = 3000;
-
-//API key
-// const API_KEY = process.env.e71e981fb1330c9361d35b9f8a2bc4b3;
 
 //Database setup
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -29,29 +23,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// app.get('/github/*', (req, res) => {
-//   console.log('Routing a GitHub AJAX request for ', req.params[0]);
-//   const url = `https://api.github.com/${req.params[0]}`;
-//   superagent.get(url)
-//     .set(`Authorization`, `token ${process.env.GITHUB_TOKEN}`)
-//     .then(
-//       repos => {
-//         console.log(repos.text);
-//         res.send(repos.text)
-//       },
-//       err => res.send(err)
-//     )
-// })
+// app Queries
 app.post('/api/v1/users', (req, res) => {
-  console.log(req.user)
   client.query(
-    `INSERT INTO users(username, password, vote_counter)
-    VALUES($1, $2, $3);`
-    [
-      req.body.username,
-      req.body.password,
-      req.body.vote_counter
-    ]
+    `INSERT INTO users(username, password)
+    VALUES($1, $2) ON CONFLICT DO NOTHING;`,
+    [req.body.username, req.body.password]
   )
     .then( function () {
       res.send('insert complete')
@@ -63,7 +40,6 @@ app.post('/api/v1/users', (req, res) => {
 
 app.get('/api/v1/users', (req, res) => {
   client.query(`SELECT * FROM users;`)
-  // .then(console.log(results))
   .then(results => res.send(results.rows))
   .catch(console.error);
 }); 
@@ -82,26 +58,6 @@ app.put('/api/v1/users/:id', (req, res) => {
     console.error(err);
   })
 })
-
-//API ENDPOINTS
-// app.get('/api endpoint', (req, res) => res.send(TOKEN === parseInt(req.query.token)))
-
-// superagent.get(url)
-//   .query({'q': query})
-//   .query({'key': API_KEY})
-//   .then(response => response.body.items.map(not sure what to map yet))
-
-//application middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-
-//api inputs
-
-
-//Add app.gets here with our API
-// for later
-//app.get()
 
 //botton of file
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
